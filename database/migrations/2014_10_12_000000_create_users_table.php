a1b94dc68a8d7a30f0f2b8e3b809211f863acd9f
sigaeasu <sigikemahduta@gmail.com>
2021-11-15 18:29:57 +0700

    add database migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('username')->nullable();
+            $table->string('password')->nullable();
+            $table->string('email')->nullable();
+            $table->dateTime('birth_date');
+            $table->string('address');
+            $table->unsignedInteger('active');
             $table->timestamps();
         });
     }
